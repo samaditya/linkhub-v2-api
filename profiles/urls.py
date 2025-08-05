9ad011eb8a9645551f6ca7ec597a4f2e22a98ccd
samaditya jatar <samadityaj@gmail.com>
2025-08-06 00:06:35 +0530

    added -me endpoint

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,7 @@
 # profiles/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProfileViewSet, LinkViewSet,UserCreateView
+from .views import ProfileViewSet, LinkViewSet,UserCreateView, ProfileMeView
 
 router = DefaultRouter()
 router.register(r'profiles', ProfileViewSet, basename='profile')
@@ -9,4 +9,5 @@ router.register(r'links', LinkViewSet, basename='link')
 
 urlpatterns = router.urls + [
     path('register/', UserCreateView.as_view(), name='user-register'),
+     path('profiles/me/', ProfileMeView.as_view(), name='profile-me'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -4,12 +4,20 @@ from rest_framework import viewsets, generics
 from django.contrib.auth.models import User
 from .models import Profile, Link
 from .serializers import ProfileSerializer, LinkSerializer,UserCreateSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly,isAuthenticated
 from .permissions import IsOwnerOrReadOnly
 
 # We will add permissions later
 # from rest_framework.permissions import IsAuthenticated
 # from .permissions import IsOwnerOrReadOnly
+
+class ProfileMeView(generics.RetrieveUpdateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user.profile
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
